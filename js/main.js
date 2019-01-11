922dff36b0ba5c1bca2d67b92528baa00abaaa5c
dumluregn <dumluregn@gmail.com>
2019-01-11 19:10:19 +0100

    Anchors: added smooth scroll effect

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$('a[href*="#"]')
+    .click(function(event) {
+        var target = $(this.hash);
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    });
