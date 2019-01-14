df5bf5c9001893ad13726fb99db1e39c5ddb8055
dumluregn <dumluregn@gmail.com>
2019-01-14 14:26:44 +0100

    Navbar: made 'sticky' (remains visible after scrolling down)

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
+const NAV = document.querySelector('#navbar');
+let navbarPosition = NAV.offsetTop;
+
 $('a[href*="#"]')
     .click(function(event) {
         var target = $(this.hash);
         event.preventDefault();
         $('html, body').animate({
-            scrollTop: target.offset().top
+            scrollTop: (target.offset().top - NAV.offsetHeight)
         }, 1000);
     });
+
+window.addEventListener('scroll', fixNav);
+
+function fixNav(){
+    if(window.scrollY >= navbarPosition){
+        NAV.style.position = 'fixed';
+        document.body.style.paddingTop = NAV.offsetHeight + 'px';
+    } else {
+        NAV.style.position = 'relative';
+        document.body.style.paddingTop = 0;
+    }
+}
